7afe66757c760546f27d4c041f1addcbbccf7340
ikkarti <karthick.k@tupo.sg>
2022-02-04 15:46:42 +0530

    fix: Added new vite setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,76 @@
-/* eslint-disable import/prefer-default-export */
-
-
 //components
-export {default as Tooltip} from './components/Tooltip.vue';
-export {default as Btn} from './components/buttons/Button.vue';
-export {default as BtnGroup} from './components/buttons/ButtonGroup.vue';
-export {default as DossierTable} from './components/dossier/DossierTable.vue';
-export {default as DossierSortSelector} from './components/dossier/SortSelector.vue';
-export {default as Modal} from './components/modal/Modal.vue';
-export {default as BaseIcon} from './components/BaseIcon.vue';
+import {default as Tooltip} from './components/Tooltip.vue';
+import {default as Btn} from './components/buttons/Button.vue';
+import {default as BtnGroup} from './components/buttons/ButtonGroup.vue';
+import {default as DossierTable} from './components/dossier/DossierTable.vue';
+import {default as DossierSortSelector} from './components/dossier/SortSelector.vue';
+import {default as Modal} from './components/modal/Modal.vue';
+import {default as BaseIcon} from './components/BaseIcon.vue';
 
 
 //fieldtypes
-export {default as TextField} from './components/fieldtypes/TextFieldtype.vue';
-export {default as TextAreaField} from './components/fieldtypes/TextAreaFieldtype.vue';
-export {default as SelectField} from './components/fieldtypes/SelectFieldtype.vue';
-export {default as ToggleField} from './components/fieldtypes/ToggleFieldtype.vue';
+import {default as TextField} from './components/fieldtypes/TextFieldtype.vue';
+import {default as TextAreaField} from './components/fieldtypes/TextAreaFieldtype.vue';
+import {default as SelectField} from './components/fieldtypes/SelectFieldtype.vue';
+import {default as ToggleField} from './components/fieldtypes/ToggleFieldtype.vue';
 
 
 //plugins
-export {default as Toaster, createToaster} from './plugins/toaster';
-export {default as copy} from './plugins/copyt-to-clipboard';
-export {default as Middleware} from './plugins/route-middleware';
-export {default as Dossier} from './components/dossier/Dossier.vue';
-export {mixin as CAMixin, plugin as CAPlugin, directive as CADirective} from './plugins/click-away';
-export {default as Events} from './modules/events'
+import {default as Toaster, createToaster} from './plugins/toaster';
+import {default as copy} from './plugins/copyt-to-clipboard';
+import {default as RouteMiddleware} from './plugins/route-middleware';
+import {default as Dossier} from './components/dossier/Dossier.vue';
+import {mixin as CAMixin, plugin as CAPlugin, directive as CADirective} from './plugins/click-away';
+import {default as Events} from './modules/events'
+
+
+const components = {
+    Tooltip,
+    Btn,
+    BtnGroup,
+    DossierTable,
+    DossierSortSelector,
+    Modal,
+    BaseIcon,
+    TextField,
+    TextAreaField,
+    SelectField,
+    ToggleField
+}
+
+
+const GigcodesAdmin = {
+    install(Vue) {
+        for (const prop in components) {
+            if (components.hasOwnProperty(prop)) {
+                const component = components[prop]
+                Vue.component(component.name, component)
+            }
+        }
+    }
+}
+
+export {
+    Tooltip,
+    Btn,
+    BtnGroup,
+    DossierTable,
+    DossierSortSelector,
+    Modal,
+    BaseIcon,
+    TextField,
+    TextAreaField,
+    SelectField,
+    ToggleField,
+    Toaster,
+    createToaster,
+    copy,
+    RouteMiddleware,
+    Dossier,
+    CAMixin,
+    CAPlugin,
+    CADirective,
+    Events
+}
+
+export default GigcodesAdmin
