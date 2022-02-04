6a19006bada078b49ccaffad551d98d718fe541a
ikkarti <karthick.k@tupo.sg>
2022-02-04 07:56:42 +0530

    Dossier mixin push

--- src/bootstrap/plugins.js
+++ src/bootstrap/plugins.js
@@ -2,3 +2,4 @@ export { default as Toaster } from '../plugins/toaster';
 export { createToaster as createToaster } from '../plugins/toaster';
 export { default as copy } from '../plugins/copyt-to-clipboard';
 export { default as Middleware } from '../plugins/route-middleware';
+export { default as Dossier } from '../components/dossier/Dossier';
