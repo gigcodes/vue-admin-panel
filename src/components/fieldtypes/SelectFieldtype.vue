296022fde5aeece9e9795e19b1d588a12f68df00
ikkarti <karthick.k@tupo.sg>
2022-02-07 22:33:21 +0530

    feat: Fieldtype fixes

--- src/components/fieldtypes/SelectFieldtype.vue
+++ src/components/fieldtypes/SelectFieldtype.vue
@@ -8,13 +8,14 @@
           <div class="text-sm text-gray-200">{{ tooltip }}</div>
         </tooltip>
       </div>
-      <select :id="id" class="form-select"
+      <select :id="id" class="form-select w-full"
               :autofocus="autofocus" :required="required"
               :aria-readonly="readonly" :disabled="disabled"
               @change="updateSelf($event.target.value)"
               @keyup="$emit('keyup',$event)"
               @focus="handleFocus"
               @blur="handleBlur"
+              :class="className"
       >
         <option v-if="placeholder" disabled>{{ placeholder }}</option>
         <option v-for="(option,key) in options" :value="key"
@@ -38,6 +39,27 @@ export default {
   name: "SelectField",
   props: ['options'],
   mixins: [Fieldtypes, Input],
+  computed: {
+    className() {
+      let classes = '';
+      if (this.disabled) {
+        classes = classes + 'disabled:border-gray-200 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed '
+      }
+      if (this.hasError) {
+        classes = classes + 'border-red-300 ';
+      }
+      if (this.hasSuccess) {
+        classes = classes + 'border-green-300 ';
+      }
+      if (this.suffix) {
+        classes = classes + 'pr-8 ';
+      }
+      if (this.prefix) {
+        classes = classes + 'pl-12';
+      }
+      return classes;
+    }
+  },
   setup(props, {emit}) {
     const options = ref(props.options)
     const handleFocus = (event) => {
