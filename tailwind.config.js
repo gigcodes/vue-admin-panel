ecfe5a2c32cdd0cf5c3ef5fbdf23c85141a7787b
Karthick <karthickk1996@hotmail.com>
2022-02-08 20:23:18 +0530

    fix: Typography package added

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,7 @@ module.exports = {
     plugins: [
         // eslint-disable-next-line global-require
         require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
         // add custom variant for expanding sidebar
         plugin(({addVariant, e}) => {
             addVariant('sidebar-expanded', ({modifySelectors, separator}) => {
