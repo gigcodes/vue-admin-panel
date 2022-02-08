f88bda068fb95a3ed35e8fc2834c67b24cf8b5c3
Karthick <karthickk1996@hotmail.com>
2022-02-08 19:46:51 +0530

    fix: Update Editor.vue for custom fieldtype data

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,17 +1,33 @@
 <template>
-  <editor-content :editor="editor"/>
+  <div :class="classes">
+    <div>
+      <div class="flex items-center justify-between">
+        <label class="block text-sm font-medium" :for="id">{{ name }}
+          <span class="text-red-500" v-if="required">*</span></label>
+        <tooltip class="ml-2" bg="dark" size="md" v-if="tooltip">
+          <div class="text-sm text-gray-200">{{ tooltip }}</div>
+        </tooltip>
+      </div>
+      <editor-content :editor="editor" :id="id"/>
+    </div>
+    <div class="text-xs mt-1" v-if="help">{{ help }}</div>
+    <div class="text-xs mt-1 text-green-500" v-if="hasSuccess">{{ hasSuccess }}</div>
+    <div class="text-xs mt-1 text-red-500" v-if="hasError">{{ hasError }}</div>
+  </div>
 </template>
 
 <script>
 import {useEditor, EditorContent} from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 import {watch} from 'vue'
+import FieldType from './fieldtypes/index';
 
 export default {
   name: "Editor",
   components: {
     EditorContent,
   },
+  mixins: [FieldType],
   emits: ['update:modelValue'],
   props: {
     modelValue: String,
@@ -34,7 +50,7 @@ export default {
     })
 
     watch(() => props.modelValue, value => {
-      if(editor.value.getHTML() === value){
+      if (editor.value.getHTML() === value) {
         return
       }
       editor.value.commands.setContent(value);
