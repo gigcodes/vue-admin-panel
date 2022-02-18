ff5b968a1ba4f72790f9680415cf8cbd8e63e106
Karthick <karthickk1996@hotmail.com>
2022-02-18 22:40:16 +0530

    feat: More icons added

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -6,14 +6,41 @@
 </template>
 
 <script>
+
 import AnalyticsIcon from "./icons/AnalyticsIcon.vue";
 import DashboardIcon from "./icons/DashboardIcon.vue";
 import ListIcon from "./icons/ListIcon.vue";
+import CommerceIcon from "./icons/CommerceIcon";
+import AuthenticationIcon from "./icons/AuthenticationIcon";
+import CampaignIcon from "./icons/CampaignIcon";
+import InboxIcon from "./icons/InboxIcon";
+import LoadingIcon from "./icons/LoadingIcon";
+import MessageIcon from "./icons/MessageIcon";
+import TaskIcon from "./icons/TaskIcon";
+import TeamIcon from "./icons/TeamIcon";
+import UtilityIcon from "./icons/UtilityIcon";
+import SettingIcon from "./icons/SettingIcon";
+import OnboardIcon from "./icons/OnboardIcon";
+import CalendarIcon from "./icons/CalendarIcon";
+import ComponentIcon from "./icons/ComponentIcon";
 
 const icons = {
   analytics: AnalyticsIcon,
   dashboard: DashboardIcon,
-  list: ListIcon
+  list: ListIcon,
+  commerce: CommerceIcon,
+  authentication : AuthenticationIcon,
+  campaign: CampaignIcon,
+  inbox: InboxIcon,
+  loading: LoadingIcon,
+  message: MessageIcon,
+  task: TaskIcon,
+  team: TeamIcon,
+  utility: UtilityIcon,
+  setting: SettingIcon,
+  onboard: OnboardIcon,
+  calendar: CalendarIcon,
+  component: ComponentIcon
 }
 
 export default {
