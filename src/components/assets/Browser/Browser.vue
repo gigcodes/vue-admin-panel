8aa4339e57bdb670e5b2002a8ea38d0f6fb15cbe
Karthick <karthickk1996@hotmail.com>
2022-07-14 22:56:20 +0530

    fix : access fix

--- src/components/assets/Browser/Browser.vue
+++ src/components/assets/Browser/Browser.vue
@@ -47,17 +47,21 @@
                     <div class="px-3" v-if="!assetsSelected.length">
                         <label for="search" class="sr-only">Search</label>
                         <div class="relative rounded-md shadow-sm">
-                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                            <div
+class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
                                  aria-hidden="true">
                                 <!-- Heroicon name: solid/search -->
-                                <svg class="mr-3 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg"
+                                <svg
+class="mr-3 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg"
                                      viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
+                                    <path
+fill-rule="evenodd"
                                           d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                                           clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input type="text" v-model="searchTerm" name="search" id="search"
+                            <input
+type="text" v-model="searchTerm" name="search" id="search"
                                    class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-9 sm:text-sm border-gray-300 rounded-md"
                                    placeholder="Search">
                         </div>
@@ -448,15 +452,15 @@ const subfolders = computed(() => {
 });
 
 const hasSubfolders = computed(() => {
-    return subfolders.length > 0;
+    return subfolders.value.length > 0;
 });
 
 const isEmpty = computed(() => {
-    return !hasAssets && !hasSubfolders;
+    return !hasAssets.value && !hasSubfolders.value;
 });
 
 const showSidebar = computed(() => {
-    if (!initialized) return false;
+    if (!initialized.value) return false;
 
     if (isSearching.value) return false;
 
@@ -592,7 +596,7 @@ const assetSelected = (asset) => {
     }
 
     // Completely prevent additional selections when the limit has been hit.
-    if (maxFilesReached) {
+    if (maxFilesReached.value) {
         return;
     }
 
